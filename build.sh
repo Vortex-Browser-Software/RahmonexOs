b4876f382f697a5627f088382a1ca706a5487c21
Rahmonex <rahmonextheone@gmail.com>
2021-07-30 17:28:05 +0200

    Personalize UEFI and syslinux boot

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
+iso_name=RahmonexOs
+iso_label="RAHMONEXOS_$(date +%Y%m)"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
